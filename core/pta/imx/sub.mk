605a08990282ab21a0deb95c7fb881fcf3b46f41
George Poulios <gpoulios@census-labs.com>
2025-07-03 21:47:52 +0300

    pta/imx/ocram: Introduce OCRAM allocator for user TAs
    Introduces a pseudo-TA that enables user TAs to use
    the OCRAM region 0x20518000 - 0x2051C000 (16K). The
    new PTA is incompatible with pta/imx/trusted_arm_ce.c
    (not used by us) by virtue of OCRAM region overlap (only).
    
    Provides 2 commands: PTA_OCRAM_CMD_{ALLOC, FREE}.
    
    Memory is allocated and mapped to the TA's address
    space through MMU mappings on a per-allocation basis.
    As such the SMALL_PAGE_SIZE (4K) granule is used, i.e.
    minimum allocation is 4K, even though smaller sizes are
    supported.
    
    Implements also access control:
     1) Only user TAs are allowed to use this
     2) Memory is not shareable between different TAs but
        it is shareable between different sessions
        established with the PTA by the same TA instance.
     3) Freeing cannot be performed across TA sessions,
        even those of the same TA instance.
    
    All allocations are freed automatically upon session
    close.
    
    Signed-off-by: George Poulios <gpoulios@census-labs.com>

--- core/pta/imx/sub.mk
+++ core/pta/imx/sub.mk
@@ -4,3 +4,4 @@ srcs-$(CFG_NXP_CAAM_MP_DRV) += manufacturing_protection.c
 srcs-$(CFG_NXP_CAAM_DEK_DRV) += dek_blob.c
 srcs-$(CFG_IMX_TRUSTED_ARM_CE) += trusted_arm_ce.c
 srcs-$(CFG_IMX_ELE_TEST_PTA) += ele_test.c
+srcs-y += ocram.c
